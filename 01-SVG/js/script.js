7bdae219d02d24a59f6d63f7a576ca40f915b10a
Debouille <deborah.antonietti@heig-vd.ch>
2022-03-03 16:31:57 +0100

    exo1

--- 01-SVG/js/script.js
+++ 01-SVG/js/script.js
@@ -1,11 +1,29 @@
 var turns = 0;
 
-function dot_01() {
-    if (turns === 0) {
-        turns++;
-        document.getElementById(".rect2").style.color = 'red';
-     } else if (turns === 1) {
-        turns--;
-        document.getElementById(".rect2").style.color = 'blue';
+const rectangles = document.querySelectorAll('.rectangle')
+
+rectangles.forEach(rectangle => {
+    rectangle.addEventListener('click', evt => {
+        evt = evt.target;
+        console.log(evt);
+        rectangle.classList.toggle('change')
+    })
+})
+
+
+domOn('.circle1', 'mousemove', async evt => {
+    const btn = evt.currentTarget;
+    if (btn.style.r === '60') {
+        btn.style.r = '100';
     }
+    else btn.style.r = '60';
+});
+
+
+function domForEach(selector, callback) {
+    document.querySelectorAll(selector).forEach(callback);
+}
+
+function domOn(selector, event, callback) {
+    domForEach(selector, ele => ele.addEventListener(event, callback));
 }
