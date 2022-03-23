acc602e3ef013c35ff70c34a98d3fa46463a62ac
Debouilles <nom@nom.com>
2022-03-23 14:48:45 +0100

    exo3

--- 03-d3-data/src/index.js
+++ 03-d3-data/src/index.js
@@ -1,59 +1,136 @@
-import { json } from 'd3-fetch'
+// https://jsonplaceholder.typicode.com/users
+// https://jsonplaceholder.typicode.com/posts
 
-var arrayUserPost = []
-console.log(arrayUserPost);
+import * as d3 from 'd3';
+import { json } from 'd3-fetch';
+
 Promise.all([
-  json('https://jsonplaceholder.typicode.com/users'),
-  json('https://jsonplaceholder.typicode.com/posts')
+    json('https://jsonplaceholder.typicode.com/posts'),
+    json('https://jsonplaceholder.typicode.com/users')
 ])
-.then(([postsTab, usersTab]) => {
-  console.log(usersTab);
-  console.log(postsTab);
-  usersTab.forEach(user => {
-      let compteurParUser = 0;
-      // console.log(user.name);
 
-      postsTab.forEach(post => {
-          // console.log(post.userId);
-          if (post.userId == user.id) {
-              compteurParUser++;
-          }
-      })
-      // console.log(compteurParUser);
+    // * A partir des données **users** et **posts**, créez un tableau d'objets qui a la structure suivante
+    // [
+    //     {
+    //       nom_utilisateur: 'Machin',
+    //       ville: 'Truc',
+    //       nom_companie: 'Bidule',
+    //       titres_posts: [
+    //         'Titre 1',
+    //         'Titre 2',
+    //       ]
+    //     },
+    //     // ...
+    //   ]
+    .then(([postsTab, usersTab]) => {
+        let arrayUserPost = [];
+        for (let i = 0; i < usersTab.length; i++) {
+            let arrayCurrentUser = {};
+            arrayCurrentUser["nom_utilisateur"] = usersTab[i].username;
+            arrayCurrentUser["ville"] = usersTab[i].address.city;
+            arrayCurrentUser["nom_companie"] = usersTab[i].company.name;
 
-      d3.select("body")
-          .append("div")
-          .attr('id', `div-user${user.id}`)
-      d3.select(`#div-user${user.id}`)
-          .append('p')
-          .text(`${user.name} a écrit ${compteurParUser} article(s).`)
-  });
-});
-var arrayUserPost2 = []
-console.log(arrayUserPost);
-Promise.all([
-  json('https://jsonplaceholder.typicode.com/users'),
-  json('https://jsonplaceholder.typicode.com/posts')
-])
-.then(([users, posts]) =>  {
-  console.log(users);
-  console.log(posts);
+            let userPosts = [];
+            postsTab.forEach(post => {
+                if (post.userId == usersTab[i].id) {
+                    userPosts.push(post.title)
+                }
+            });
+            arrayCurrentUser["posts"] = userPosts;
+            arrayUserPost.push(arrayCurrentUser);
+            // console.log(arrayCurrentUser);
+        }
+        // console.log(arrayUserPost);
 
-  for (let i = 0; i < users.length; i++) {
-      var arrayCurrentUser = {};
-      arrayCurrentUser["nom_utilisateur"] = users[i].username;
-      arrayCurrentUser["ville"] = users[i].address.city;
-      arrayCurrentUser["nom_companie"] = users[i].company.name;
-      
-      var userPosts = [];
-      posts.forEach(post => {
-          if (post.userId == users[i].id) {
-              userPosts.push(post.title)
-          }
-      });
-      arrayCurrentUser["posts"] = userPosts;
+        // *********************************************************
+        let graphPosts = []; // pour les besoins de la dernière partie.
+        // *********************************************************
+        // * Calculez le nombre de **posts** par **user**
+        d3.select("body")
+            .append("div")
+            .attr('id', `div-users`)
+        usersTab.forEach(user => {
+            let compteurParUser = 0;
+            // console.log(user.name);
 
-      arrayUserPost.push(arrayCurrentUser);
-  }
-});
- 
+            postsTab.forEach(post => {
+                // console.log(post.userId);
+                if (post.userId == user.id) {
+                    compteurParUser++;
+                }
+            })
+            graphPosts.push(compteurParUser);
+            d3.select(`#div-users`)
+                .append('div')
+                .attr('id', user.id)
+                .append('p')
+                .text(`${user.name} a écrit ${compteurParUser} article(s).`)
+        })
+
+        // *********************************************************
+        // * Trouvez le **user** qui a écrit le texte le plus long dans **posts.body**
+        let postLePlusLong = 'abc';
+        let postLePlusLongUserId = 0;
+        console.log(postsTab);
+        postsTab.forEach(post => {
+            // console.log(post.body.length);
+            if (postLePlusLong.length < post.body.length) {
+                postLePlusLong = post.body;
+                postLePlusLongUserId = post.userId
+            }
+        })
+        console.log(postLePlusLong);
+        console.log(postLePlusLongUserId);
+        let userPostLePlusLong = usersTab[postLePlusLongUserId - 1].name;
+
+        d3.select("body")
+            .append("div")
+            .attr('id', 'postLePlusLong')
+        d3.select('#postLePlusLong')
+            .append('p')
+            .text(`${userPostLePlusLong} a écrit le plus long post. Ce dernier disait "${postLePlusLong}". :-)`)
+
+        // *********************************************************
+        // * Dessinez un graphique en bâton en ayant sur l'axe *x* les utilisateurs et *y* le nombre de posts
+
+        let margin = { top: 20, right: 10, bottom: 60, left: 60 };
+        let width = 1500 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        d3.select("body")
+            .append("div")
+            .attr('id', 'graph')
+        let svg = d3.select("#graph")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+        let x = d3.scaleBand()
+            .domain(arrayUserPost.map(function (d) { return d["nom_utilisateur"]; }))
+            .range([1000, 0]);
+
+        let y = d3.scaleLinear()
+            .domain([0, 10])
+            .range([height, 0]);
+
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+            .attr("transform", "translate(-2,10)")
+
+        svg.selectAll("bars")
+            .data(arrayUserPost)
+            .enter()
+            .append("rect")
+            .attr("x", function (d) { return x(d["nom_utilisateur"]) + 30; })
+            .attr("y", function (d) { return y(d["posts"].length); })
+            .attr("width", "40px")
+            .attr("height", function (d) { return height - y(d["posts"].length); })
+            .attr("fill", `#${Math.floor(Math.random()*16777215).toString(16)}`)
+    })
